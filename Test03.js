78b0e1020af119d6246177dd19aad38c6dcdb525
Black280197 <121843405+Black280197@users.noreply.github.com>
2023-02-07 15:38:56 +0700

    Sửa lại bài 3 thành tổng số nguyên tố

--- Test03.js
+++ Test03.js
@@ -2,19 +2,31 @@
 
 function sumOfListPrime(x) {
     let sum = 0;
-    if (x >= 0) {
-        for (let i = 0; i <= x; i++) {
+    for (let i = 0; i <= x; i++) {
+        if (ktnt(i)) {
             sum += i;
         }
-    } else {
-        for (let j = 0; j >= x; j--) {
-            sum += j;
-        }
     }
+
     return sum;
 }
 function xuat() {
     let x = parseInt(document.getElementById("nhap").value);
     console.log(x);
     document.getElementById("result").innerHTML = sumOfListPrime(x);
+}
+
+function ktnt(n) {
+    let dem = 0;
+    if (n <= 2) {
+        return false;
+    } else {
+        for (let i = 1; i <= Math.floor(Math.sqrt(n)); i++) {
+            if (n % i === 0) {
+                dem++;
+            }
+        }
+    }
+    if (dem === 1) return true;
+    else return false;
 }
